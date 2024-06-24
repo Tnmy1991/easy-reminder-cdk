c6b2237443ba86120e5c7650fd9e7869d4db26d7
Tonmoy Malik <tonmoy.malik@gmail.com>
2024-06-24 11:59:01 +0530

    inital commit

---
+++ test/easy-reminder-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as EasyReminderCdk from '../lib/easy-reminder-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/easy-reminder-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new EasyReminderCdk.EasyReminderCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
